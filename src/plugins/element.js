3db6da176e8af86876cf41f8b42f15f34301f897
ryan <rq.yannn@gmail.com>
2021-07-20 17:33:39 +0800

    创建登录组件页面
    less，less loader依赖
    样式表，elementUI表单预校验，重置

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
-import Element from 'element-ui'
+import Element, { Button, Form, FormItem, Input } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Element)
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
