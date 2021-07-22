b76cf3a8ea30fb51273df41706cf39476971df8f
ryan <rq.yannn@gmail.com>
2021-07-22 11:38:11 +0800

    登出功能
    remove token

--- src/router/index.js
+++ src/router/index.js
@@ -22,4 +22,19 @@ const router = new VueRouter({
   ]
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径，from 从哪个路径跳转来，next 是一个函数，表示放行
+  // 1、next()放行    2、next('/login')强制跳转
+  if (to.path === '/login') {
+    return next()
+  }
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('/login')
+  }
+  next()
+})
+
 export default router
